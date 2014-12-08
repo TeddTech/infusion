44a7e2b5a5e651117c6578cf8551a2a6300458fe
Cindy Qi Li <cli@ocadu.ca>
2014-12-08 10:29:11 -0500

    TEMP: The first draft of the PGA demo. Requires FLUID-230 (http://issues.fluidproject.org/browse/FLOE-230) to be resolved for the next button to work.

---
+++ demos/pga/js/pga.js
@@ -0,0 +1,72 @@
+var fluid = fluid_2_0 || {};
+
+(function ($, fluid) {
+    "use strict";
+
+    fluid.registerNamespace("fluid.pga");
+
+    fluid.defaults("fluid.pga", {
+        gradeNames: ["fluid.viewRelayComponent", "fluid.prefs.prefsEditorLoader", "autoInit"],
+        selectors: {
+            prefsEditor: ".flc-panels",
+            nextButton: ".flc-next"
+        },
+        components: {
+            prefsEditor: {
+                container: "{that}.container",
+                options: {
+                    listeners: {
+                        onReady: {
+                            listener: "{pga}.events.onReady",
+                            args: "{pga}"
+                        }
+                    }
+                }
+            }
+        },
+        model: {
+            currentPanel: 1
+        },
+        events: {
+            onReady: null
+        },
+        listeners: {
+            "onReady.bindNextButton": {
+                "this": "{that}.dom.nextButton",
+                "method": "click",
+                "args": ["{that}.nextPanel"]
+            }
+        },
+        invokers: {
+            "nextPanel": {
+                funcName: "fluid.pga.nextPanel",
+                args: ["{that}"]
+            }
+        }
+    });
+
+    fluid.pga.nextPanel = function (that) {
+        var nextPanel = ++that.model.currentPanel;
+        that.applier.change("currentPanel", nextPanel);
+    };
+
+    // The grade component for PAG panels
+    fluid.defaults("fluid.PGAPanel", {
+        gradeNames: ["fluid.standardRelayComponent", "autoInit"],
+        currentPanel: 5,
+        model: {
+            currentPanel: "{fluid.pga}.model.currentPanel"
+        },
+        modelListeners: {
+            currentPanel: {
+                listener: "fluid.PGAPanel.presentPanel",
+                args: ["{change}.value", "{that}.options.order", "{that}.container"]
+            }
+        }
+    });
+
+    fluid.PGAPanel.presentPanel = function (currentPanel, order, container) {
+        container[(order === currentPanel) ? "show" : "hide"]();
+    };
+
+})(jQuery, fluid);
